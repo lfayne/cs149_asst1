60e406e6746a3b5de397824379a0f96d6a4c63c1
Matt Borges Wolff <mbwolff@myth60.stanford.edu>
2024-10-02 17:58:41 -0700

    Made more progress.

--- prog4_sqrt/sqrtSerial.cpp
+++ prog4_sqrt/sqrtSerial.cpp
@@ -33,11 +33,11 @@ void sqrtAVX2(int N,
                 float output[])
 {
 
-    static const float kThreshold = 0.00001f;
-    static const float one = 1.f;
-    __m256 kThresholdVec = _mm256_broadcast_ss(&kThreshold);
-    __m256 oneVec = _mm256_broadcast_ss(&one);
-    __m256 sign_bit = _mm256_set1_ps(-0.0f);
+    __m256 kThresholdVec = _mm256_set1_ps(0.00001f);;
+    __m256 threeVec = _mm256_set1_ps(3.f);
+    __m256 oneVec = _mm256_set1_ps(1.f);
+    __m256 halfVec = _mm256_set1_ps(0.5f);
+    __m256 signVec = _mm256_set1_ps(-0.0f);
 
     for (int i=0; i<N; i+=8) {
 
@@ -50,9 +50,19 @@ void sqrtAVX2(int N,
         error = _mm256_sub_ps(error, oneVec);
 
         // Abs error
-        error = _mm256_andnot_ps(sign_bit, error);
+        error = _mm256_andnot_ps(signVec, error);
 
+        __m256 comparisonVec = _mm256_cmp_ps(error, kThresholdVec, _CMP_GT_OS);
+        char comparisonBits = _mm256_movemask_ps(comparisonVec);  // If all 0, then the while loop is finished
+        while (comparisonBits != 0) {
+            __m256 newGuess = _mm256_mul_ps(guess, guess);
+            newGuess = _mm256_mul_ps(newGuess, guess);
+            newGuess = _mm256_mul_ps(x, guess);
+            newGuess = _mm256_sub_ps(_mm256_mul_ps(threeVec, guess), newGuess);
+            newGuess = _mm256_mul_ps(newGuess, halfVec);
+        }
         while (error > kThreshold) {
+
             guess = (3.f * guess - x * guess * guess * guess) * 0.5f;
             error = fabs(guess * guess * x - 1.f);
         }
